7e49d95dab829036d032a01dee6cdc119848b518
Quinn Keaveney <qkeave@gmail.com>
2026-04-07 17:08:09 -0700

    Rename to camelCase: @liiift-studio/textBreath

--- site/src/components/CopyInstall.tsx
+++ site/src/components/CopyInstall.tsx
@@ -3,8 +3,8 @@
 // Click-to-copy npm install snippet with a link to the npm package page
 import { useState } from "react"
 
-const CMD = "npm install @liiift-studio/text-breath"
-const NPM_URL = "https://www.npmjs.com/package/@liiift-studio/text-breath"
+const CMD = "npm install @liiift-studio/textBreath"
+const NPM_URL = "https://www.npmjs.com/package/@liiift-studio/textBreath"
 
 /** Displays the install command, copies it to clipboard on click, and links to npm */
 export default function CopyInstall() {
